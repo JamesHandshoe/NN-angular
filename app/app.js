8fe7c59cb4a2a0d86e4b5c59edad04a75cf8cf1f
James <jhandshoe@sbcglobal.net>
2016-07-28 14:29:11 -0400

    added http and created a json file to access data

--- app/app.js
+++ app/app.js
@@ -18,7 +18,7 @@ myNinjaApp.config(['$routeProvider', function($routeProvider){
 
 
 //controls a section of DOM
-myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+myNinjaApp.controller('NinjaController', ['$scope', '$http', function($scope, $http){
 
 	$scope.removeNinja = function(ninja){
 		var removedNinja = $scope.ninjas.indexOf(ninja);
@@ -38,41 +38,12 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 		$scope.newninja.rate = "";
 	};
 
-	$scope.ninjas = [
-		{
-			name: "Yoshi",
-			belt: "Green",
-			rate: 50,
-			available: true,
-			thumb: "content/img/yoshi.png"
-		},
-		{
-			name: "Crystal",
-			belt: "Yellow",
-			rate: 30,
-			available: true,
-			thumb: "content/img/crystal.jpg"
-		},
-		{
-			name: "Ryu",
-			belt: "Orange",
-			rate: 10,
-			available: true,
-			thumb: "content/img/ryu.png"
-		},
-		{
-			name: "Shaun",
-			belt: "Black",
-			rate: 1000,
-			available: true,
-			thumb: "content/img/shaun.png"
-		}
-	];
+	$http.get('data/ninjas.json').success(function(data){
+		$scope.ninjas = data;
+	});
+
 }]);
 
 
-//.run runs after config and as application runs
-myNinjaApp.run(function(){
 
-});
 
