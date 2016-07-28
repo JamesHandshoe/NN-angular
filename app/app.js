cef8a2b4d9792aeed22ecdc0a22b47d90a714e1e
James <jhandshoe@sbcglobal.net>
2016-07-28 14:08:18 -0400

    added ng route and page routing

--- app/app.js
+++ app/app.js
@@ -1,5 +1,21 @@
 //this creates the app module
-var myNinjaApp = angular.module('myNinjaApp', []);
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+
+//.config runs before application runs
+myNinjaApp.config(['$routeProvider', function($routeProvider){
+
+	$routeProvider
+		.when('/home', {
+			templateUrl: "views/home.html"
+		})
+		.when('/directory', {
+			templateUrl: "views/directory.html",
+			controller: 'NinjaController'
+		}).otherwise({
+			redirectTo: '/home'
+		});
+}]);
+
 
 //controls a section of DOM
 myNinjaApp.controller('NinjaController', ['$scope', function($scope){
@@ -53,13 +69,6 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 		}
 	];
 }]);
-
-
-
-//.config runs before application runs
-myNinjaApp.config(function(){
-
-});
 
 
 //.run runs after config and as application runs
