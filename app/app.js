aae39882dc0796bc4b4353986426cdc42fef7ee6
James <jhandshoe@sbcglobal.net>
2016-07-28 10:43:17 -0400

    added controllers

--- app/app.js
+++ app/app.js
@@ -1,6 +1,14 @@
 //this creates the app module
 var myNinjaApp = angular.module('myNinjaApp', []);
 
+//controls a section of DOM
+myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+
+	$scope.message = "hey whats up";
+
+	$scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun'];
+}]);
+
 //.config runs before application runs
 myNinjaApp.config(function(){
 
