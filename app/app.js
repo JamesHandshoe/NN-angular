c9b4dbd13522909df0243734ad56b9a12d9ffeae
James <jhandshoe@sbcglobal.net>
2016-07-28 10:32:51 -0400

    added app.js and started modules

---
+++ app/app.js
@@ -0,0 +1,14 @@
+//this creates the app module
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+//.config runs before application runs
+myNinjaApp.config(function(){
+
+});
+
+
+//.run runs after config and as application runs
+myNinjaApp.run(function(){
+
+});
+
